aabe860d5e7cc0ef4a5692675f72f77d2a467d33
Megh Khaire <meghkhaire@gmail.com>
2024-09-26 21:22:45 -0400

    Setup core ns

--- src/chroma_clj/core.clj
+++ src/chroma_clj/core.clj
@@ -1,7 +1,18 @@
 (ns chroma-clj.core
-  (:gen-class))
+  "Core API functions for interacting with ChromaDB."
+  (:gen-class)
+  (:require [chroma-clj.config :refer [config]]
+            [chroma-clj.utils :as util]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn get-version
+  "Retrieve the ChromaDB server version."
+  []
+  (util/make-chroma-request @config :get "version" {}))
+
+
+(defn health-check
+  "Check if the ChromaDB server is active and responding."
+  []
+  (util/make-chroma-request @config :get "heartbeat" {}))
+
