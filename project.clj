628f672b6f7259f3f1a3f4f1b0b27f62bc852a01
Megh Khaire <meghkhaire@gmail.com>
2024-09-26 21:23:49 -0400

    Setup project config and dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject chroma-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot chroma-clj.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+(defproject chroma-clj "0.1.0-alpha"
+  :author "Megh Khaire"
+  :description "Clojure client for ChromaDB"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies
+  [[org.clojure/clojure "1.11.2"]
+   [cheshire "5.12.0"]
+   [clj-http "3.12.3"]])
