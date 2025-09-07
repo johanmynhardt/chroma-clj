03fea3af095e79f28f5a5be55c4f99dd7318ebcb
Johan Mynhardt <johan.mynhardt@global.ntt>
2025-09-07 10:32:58 +0200

    refactor(src/chroma_clj/core.clj, src/chroma_clj/utils.clj): update API endpoints and request handling
    - rename `health-check` to `heartbeat`
    - add new function `auth-identity` for retrieving authentication identity
    - update `get-version` to use the new API endpoint
    - add new functions `healthcheck`, `databases` for additional server checks and database operations
    - update `generate-url` to use `/api/v2/` prefix
    - refactor `make-chroma-request` to handle errors more gracefully

--- src/chroma_clj/core.clj
+++ src/chroma_clj/core.clj
@@ -4,15 +4,34 @@
   (:require [chroma-clj.config :refer [config]]
             [chroma-clj.utils :as util]))
 
+(defn auth-identity
+  []
+  (util/make-chroma-request @config :get "auth/identity" {}))
 
 (defn get-version
   "Retrieve the ChromaDB server version."
   []
   (util/make-chroma-request @config :get "version" {}))
 
-
-(defn health-check
+(defn heartbeat
   "Check if the ChromaDB server is active and responding."
   []
   (util/make-chroma-request @config :get "heartbeat" {}))
 
+(defn healthcheck
+  "Check if the ChromaDB server is active and responding."
+  []
+  (util/make-chroma-request @config :get "healthcheck" {}))
+
+(defn databases []
+  (util/make-chroma-request @config :get "" {}))
+
+(comment
+
+  (auth-identity)
+  (get-version)
+  (heartbeat)
+  (healthcheck)
+
+  (databases)
+  )
--- src/chroma_clj/utils.clj
+++ src/chroma_clj/utils.clj
@@ -1,28 +1,33 @@
 (ns chroma-clj.utils
   "Core API functions for interacting with ChromaDB."
   (:require [cheshire.core :as json]
-            [clj-http.client :as http])
+            [org.httpkit.client :as http])
   (:refer-clojure :exclude [get update count]))
 
 
 (defn generate-url
   "Construct the full URL for a given API `path` using the current `config`."
   [config path]
-  (str (:uri config) "/api/v1/" path))
+  (str (:uri config) "/api/v2/" path))
 
 
 (defn- handle-response
   "Process the HTTP `response`, throwing an exception if the status is not successful."
   [response]
-  (let [{:keys [status body]} response]
-    (if (<= 200 status 299)
-      body
-      (throw (ex-info "HTTP Error"
-                      {:status status
-                       :body   (try
-                                 (json/parse-string body true)
-                                 (catch Exception _
-                                   body))})))))
+  (let [{:keys [error status body]} (deref response)]
+    (cond error
+          (throw error)
+
+          (<= 200 status 299)
+          (json/parse-string body keyword)
+
+          :else
+          (throw (ex-info
+                  "HTTP Error"
+                  {:status status
+                   :body (try
+                           (json/parse-string body true)
+                           (catch Exception _ body))})))))
 
 
 (defn make-chroma-request
@@ -40,6 +45,6 @@
               :body            (when body (json/generate-string body))
               :socket-timeout  timeout
               :conn-timeout    timeout
-              :as              :json-string-keys
+             ; :as              :json-string-keys
               :throw-exceptions false}]
     (handle-response (http/request opts))))
