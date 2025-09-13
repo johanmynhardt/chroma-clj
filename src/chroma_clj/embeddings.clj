d0bf80c5354f5af30aac8ebbed636b01bb243bbb
Johan Mynhardt <johanmynhardt@gmail.com>
2025-09-13 07:21:57 +0200

    refactor(src/chroma_clj/embeddings.clj): update API paths and request configuration
    - refactor `:require` to use `:refer` for clarity
    - update API paths to include tenant, database, and collection details
    - adjust function parameters and request configuration to match new path structure

--- src/chroma_clj/embeddings.clj
+++ src/chroma_clj/embeddings.clj
@@ -1,5 +1,5 @@
 (ns chroma-clj.embeddings
-  (:require [chroma-clj.config :only [config]]
+  (:require [chroma-clj.config :refer [config]]
             [chroma-clj.utils :as util]))
 
 
@@ -7,33 +7,39 @@
   "Add embeddings to a collection. `embeddings` is a sequence of maps with keys:
    `:id`, `:embedding`, `:metadata`, and `:document`."
   [collection-id embeddings & {:keys [upsert?] :or {upsert? false}}]
-  (let [path (str "collections/" collection-id (if upsert? "/upsert" "/add"))
+  (let [path "tenants/{tenant}/databases/{database}/collections/{collection-id}/{embedding-action}"
         data (let [ids        (mapv :id embeddings)
-                   embeddings (mapv :embedding embeddings)
+                   embeddings' (mapv :embedding embeddings)
                    metadatas  (mapv :metadata embeddings)
                    documents  (mapv :document embeddings)]
                (cond-> {}
                  (seq ids)        (assoc :ids ids)
-                 (seq embeddings) (assoc :embeddings embeddings)
+                 (seq embeddings') (assoc :embeddings embeddings')
                  (seq metadatas)  (assoc :metadatas metadatas)
                  (seq documents)  (assoc :documents documents)))]
-    (util/make-chroma-request @config :post path {:body data})))
+    (util/make-chroma-request
+     (assoc @config
+            :collection-id collection-id
+            :embedding-action (if upsert? "upsert" "add"))
+     :post path {:body data})))
 
 
 (defn update-embeddings
   "Update embeddings in a collection."
   [collection-id embeddings]
-  (let [path (str "collections/" collection-id "/update")
+  (let [path "tenants/{tenant}/databases/{database}/collections/{collection-id}/update"
         data (let [ids        (mapv :id embeddings)
-                   embeddings (mapv :embedding embeddings)
+                   embeddings' (mapv :embedding embeddings)
                    metadatas  (mapv :metadata embeddings)
                    documents  (mapv :document embeddings)]
                (cond-> {}
                  (seq ids)        (assoc :ids ids)
-                 (seq embeddings) (assoc :embeddings embeddings)
+                 (seq embeddings') (assoc :embeddings embeddings')
                  (seq metadatas)  (assoc :metadatas metadatas)
                  (seq documents)  (assoc :documents documents)))]
-    (util/make-chroma-request @config :post path {:body data})))
+    (util/make-chroma-request
+     (assoc @config :collection-id collection-id)
+     :post path {:body data})))
 
 
 (defn get-embeddings
@@ -42,14 +48,16 @@
                     :or {include [:metadatas :documents]
                          offset 0
                          limit 100}}]
-  (let [path (str "collections/" collection-id "/get")
+  (let [path "tenants/{tenant}/databases/{database}/collections/{collection-id}/get"
         body (cond-> {}
                ids             (assoc :ids ids)
                where           (assoc :where where)
                where-document  (assoc :where_document where-document)
                include         (assoc :include include))
         params {:offset offset :limit limit}]
-    (util/make-chroma-request @config :post path {:params params :body body})))
+    (util/make-chroma-request
+     (assoc @config :collection-id collection-id)
+     :post path {:params params :body body})))
 
 
 (defn delete-embeddings
@@ -57,9 +65,11 @@
   [collection-id & {:keys [ids where where-document]}]
   (when-not (or ids where where-document)
     (throw (ex-info "At least one of `ids`, `where`, or `where-document` must be provided." {})))
-  (let [path (str "collections/" collection-id "/delete")
+  (let [path "tenants/{tenant}/databases/{database}/collections/{collection-id}/delete"
         body (cond-> {}
                ids             (assoc :ids ids)
                where           (assoc :where where)
                where-document  (assoc :where_document where-document))]
-    (util/make-chroma-request @config :post path {:body body})))
+    (util/make-chroma-request
+     (assoc @config :collection-id collection-id)
+     :post path {:body body})))
