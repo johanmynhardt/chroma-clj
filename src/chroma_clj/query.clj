4ee373158391ee7ab998c7197049149506f60478
Johan Mynhardt <johanmynhardt@gmail.com>
2025-09-13 07:26:49 +0200

    refactor(src/chroma_clj/query.clj): update query paths and request configuration
    - replace `:only` with `:refer` for `config` to simplify namespace usage
    - update query path to include tenant, database, and collection-id parameters
    - adjust `query-batch` function to use updated path and ensure proper configuration association

--- src/chroma_clj/query.clj
+++ src/chroma_clj/query.clj
@@ -1,5 +1,5 @@
 (ns chroma-clj.query
-  (:require [chroma-clj.config :only [config]]
+  (:require [chroma-clj.config :refer [config]]
             [chroma-clj.utils :as util]))
 
 
@@ -9,14 +9,16 @@
    & {:keys [where where-document include num-results]
       :or {include [:metadatas :documents :distances]
            num-results 10}}]
-  (let [path (str "collections/" collection-id "/query")
+  (let [path "tenants/{tenant}/databases/{database}/collections/{collection-id}/query"
         body {:query_embeddings [query-embedding]
               :n_results        num-results
               :include          include}
         body (cond-> body
                where           (assoc :where where)
                where-document  (assoc :where_document where-document))]
-    (util/make-chroma-request @config :post path {:body body})))
+    (util/make-chroma-request
+     (assoc @config :collection-id collection-id)
+     :post path {:body body})))
 
 
 (defn query-batch
@@ -25,11 +27,13 @@
    & {:keys [where where-document include num-results]
       :or {include [:metadatas :documents :distances]
            num-results 10}}]
-  (let [path (str "collections/" collection-id "/query")
+  (let [path "tenants/{tenant}/databases/{database}/collections/{collection-id}/query"
         body {:query_embeddings query-embeddings
               :n_results        num-results
               :include          include}
         body (cond-> body
                where           (assoc :where where)
                where-document  (assoc :where_document where-document))]
-    (util/make-chroma-request @config :post path {:body body})))
+    (util/make-chroma-request
+     (assoc @config :collection-id collection-id)
+     :post path {:body body})))
